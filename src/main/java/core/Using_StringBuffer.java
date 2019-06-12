c791ac6401b68ea4bb55e3587823f0192128e843
Yul Per <yuliiaperedrii.qa@gmail.com>
2019-06-12 16:08:24 -0700

    Initial commit

---
+++ src/main/java/core/Using_StringBuffer.java
@@ -0,0 +1,12 @@
+package core;
+
+public class Using_StringBuffer {
+	public static void main(String[] args) {
+        final long start = System.nanoTime();
+        StringBuffer sb = new StringBuffer("a");
+        int max = 1_000_000;
+        for (int i = 0; i < max; i++) {sb = sb.append("b");}
+        final long finish = System.nanoTime();
+  System.out.println("Using [StringBuffer] " + max + " times, Elapsed time: " + (finish - start) / 1_000_000.0 + " ms");
+}
+}
